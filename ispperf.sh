b0b504692a03f8c3e8137df2d8105788cbb6b67d
Jay Goldberg <jaymgoldberg@gmail.com>
2019-03-08 12:03:58 -0800

    Remove some bash-isms, formatting cleanup, var renaming
    * Remove backtick syntax
    * Remove unsupported bash `function` keyword
    * Expand some var names (I know we're trying to be succinct, but...)
    * fix indenting on loops
    * `; do` syntax on loops, `; then` syntax on `if`

--- ispperf.sh
+++ ispperf.sh
@@ -1,27 +1,8 @@
 #!/bin/bash
 
-# The MIT License
+# The MIT License, Copyright (c) 2010, MaDeuce
 #
-# Copyright (c) 2010, MaDeuce
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-# 
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-# THE SOFTWARE.
-
+# =========================================================================
 # This command measures latency and bandwidth of an internet connection from the command line.
 # It uses hosts that participate in speedtest.net.  Output is in the form of a csv file.
 # As designed, speedtest.net can be used to test latency and bandwidth only from a browser
@@ -52,16 +33,16 @@ HOSTS="$AUSTIN_TX $PALOALTO_CA $MIAMI_FL $BOSTON_MA $CHICAGO_IL $PORTLAND_OR $RE
 
 
 # output a timestamp
-function tm {
+write_timestamp () {
     date "+%m%d,%H:%M:%S," | tr -d "\n"
 }
 
-function r13 {
+r13 () {
     # 13 digit random number
     echo "${RANDOM}${RANDOM}${RANDOM}" | cut -c -13 -
 }
 
-function title {
+title () {
 cat<<EOF
 #p = ping record
 #p,***d,HH:MM:SS,min,avg,max,stddev
@@ -78,102 +59,98 @@ EOF
 # I'll redo the script in python before I do that, I think.
 
 # test download speed
-function download {
-    HST=$1
+download () {
+    HOST=$1
     SIZE=$2
     MAXSECS=$3
     FMT="%{time_total},%{time_namelookup},%{time_connect},%{time_starttransfer},%{size_download},%{speed_download}\n"
-    RND=`r13`
-    URL="$HST/random${SIZE}x${SIZE}.jpg?x=${RND}-1"
-    HST=`echo $HST | sed -e s/\\\/.*$//g -e s/:.*$//g`
+    RND=$(r13)
+    URL="$HOST/random${SIZE}x${SIZE}.jpg?x=${RND}-1"
+    HOST=$(echo $HOST | sed -e s/\\\/.*$//g -e s/:.*$//g)
     echo -n 'd,'
-    tm
-    echo -n "${HST},"
+    write_timestamp
+    echo -n "${HOST},"
     curl -m ${MAXSECS} -s -w "$FMT" $URL -o /dev/null
 }
 
 # test upload speed
-function upload {
-    HST=$1
+upload () {
+    HOST=$1
     SIZE=$2
     MAXSECS=$3
     # get random data from openssl. '-hex' will output two characters for each byte, therefore desired
     # size has to be divided by two. this is only approximate, but that's ok.
-    DATA=`openssl rand $((SIZE/2)) -hex`
+    DATA=$(openssl rand $((SIZE/2)) -hex)
     FMT="%{time_total},%{time_namelookup},%{time_connect},%{time_starttransfer},%{size_upload},%{speed_upload}\n"
-    RND=`r13`
-    URL="$HST/upload.php?x=0.${RND}"
-    HST=`echo $HST | sed -e s/\\\/.*$//g -e s/:.*$//g`
+    RND=$(r13)
+    URL="$HOST/upload.php?x=0.${RND}"
+    HOST=$(echo $HOST | sed -e s/\\\/.*$//g -e s/:.*$//g)
     echo -n 'u,'
-    tm
-    echo -n "${HST},"
+    write_timestamp
+    echo -n "${HOST},"
     curl -m ${MAXSECS} -s -w "$FMT" -d $DATA -o /dev/null $URL
 }
 
 # test http latency by getting small file
-function latency {
-    HST=$1
+latency () {
+    HOST=$1
     MAXSECS=$2
-    RND=`r13`
-    URL="$HST/latency.txt?x=${RND}"
-    HST=`echo $HST | sed -e s/\\\/.*$//g -e s/:.*$//g`
+    RND=$(r13)
+    URL="$HOST/latency.txt?x=${RND}"
+    HOST=$(echo $HOST | sed -e s/\\\/.*$//g -e s/:.*$//g)
     echo -n 'l,'
-    tm
-    echo -n "${HST},"
+    write_timestamp
+    echo -n "${HOST},"
     FMT="%{time_total},%{time_namelookup},%{time_connect},%{time_starttransfer},%{size_download},%{speed_download}\n"
     curl -m ${MAXSECS} -s -w "$FMT" $URL -o /dev/null
 }
 
 # test IP latency via ping
-function pingit {
-    HST=$1
+pingit () {
+    HOST=$1
     MAXSECS=$2
     # just want hostname -- get rid of any http url fragment or port number
-    HST=`echo $HST | sed -e s/\\\/.*$//g -e s/:.*$//g`
+    HOST=$(echo $HOST | sed -e s/\\\/.*$//g -e s/:.*$//g)
     echo -n "p,"
-    tm
-    echo -n "${HST},"
+    write_timestamp
+    echo -n "${HOST},"
     # ping the host. hang on to the results so return code can be kept
-    PV=`ping -t${MAXSECS} -c3 -Qq $HST`
-    RC=$?
-    if [ $RC -eq 0 ]
-    then
-    # ping was successful
-    # output min/avg/max/stdev
-    echo $PV | fgrep round-trip | sed s/^.*=//g | sed -e 's/\//,/g' -e 's/ //g' -e 's/ms//g'
+    PING_VALUE=$(ping -t${MAXSECS} -c3 -Qq $HOST)
+    exit_code=$?
+    if [ $exit_code -eq 0 ]; then
+        # ping was successful
+        # output min/avg/max/stdev
+        echo $PING_VALUE | fgrep round-trip | sed s/^.*=//g | sed -e 's/\//,/g' -e 's/ //g' -e 's/ms//g'
     else
-    # some speedtest.net hosts do not respond to pings
-    # ping was unsuccessful
-    echo "0,0,0,0"
+        # some speedtest.net hosts do not respond to pings
+        # ping was unsuccessful
+        echo "0,0,0,0"
     fi
 }
 
 # They use jpeg images with random content as a paylod for testing downloads.  The images are all
 # square (i.e., 'n x n').  There are nine fixed sizes of images.
-DSIZES="350 500 1000 1500 2000 2500 3000 3500 4000"
+DOWNLOAD_SIZES="350 500 1000 1500 2000 2500 3000 3500 4000"
 
 # For some reason, they only use two sizes of payloads for upload testing.  25097 and 151325 bytes.
 # The payloads are simply random strings which are generated on the client side.  I don't think that their
 # exact size is important, nor do I think anything would prevent you from adding your own sizes.
-USIZES="25097 151325"
+UPLOAD_SIZES="25097 151325"
 
 title
-while :
-do
-    for HOST in $HOSTS
-    do
+
+while true; do
+    for HOST in $HOSTS; do
         TMOUT=30                             # giveup after 30 seconds
         latency $HOST $TMOUT
-        pingit  $HOST $TMOUT
-        for SIZE in $DSIZES
-        do
-        TMOUT=$((60*10))                 # giveup after 10 minutes
-        download $HOST $SIZE $TMOUT
+        pingit $HOST $TMOUT
+        for SIZE in $DOWNLOAD_SIZES; do
+            TMOUT=$((60*10))                 # giveup after 10 minutes
+            download $HOST $SIZE $TMOUT
         done
-        for SIZE in $USIZES
-        do
-        TMOUT=$((60*10))                 # giveup after 10 minutes
-        upload $HOST $SIZE $TMOUT
+        for SIZE in $UPLOAD_SIZES; do
+            TMOUT=$((60*10))                 # giveup after 10 minutes
+            upload $HOST $SIZE $TMOUT
         done
     done
 done
